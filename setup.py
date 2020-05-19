05a84b4eddbcccda5655ba3d4a48fb74a31e2d29
Ravinder <hiravinder@gmail.com>
2020-05-19 12:22:04 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Setting up a project to create data science pipeline',
+    author='Ravinder',
+    license='MIT',
+)
